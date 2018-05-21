08b691b08416c30ca9e66416a0d6f9aa8ff23677
Rodrigo Valente <rodrigovalente@hotmail.com>
2018-05-21 20:08:05 -0300

    refresh

--- salesapp/admin.py
+++ salesapp/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from salesapp.models import Customers, Sellers
+from salesapp.models import Customers
 
 class CustomersAdmin(admin.ModelAdmin):
     list_display = ['soldToName', 'sellerName']
     list_filter = ['sellerName', 'products']
 
 admin.site.register(Customers, CustomersAdmin)
-admin.site.register(Sellers)
--- salesapp/models.py
+++ salesapp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
+class Groups(models.Model):
+    group = models.CharField(max_length=100)
+
 class Customers(models.Model):
     soldToNumber = models.CharField(max_length=10)
     soldToName = models.CharField(max_length=80)
@@ -14,7 +18,6 @@ class Customers(models.Model):
     sellerName = models.CharField(max_length=80)
     cmscNumber = models.CharField(max_length=10)
     cmscName = models.CharField(max_length=80)
-    group = models.CharField(max_length=100, blank=True)
 
     def get_absolute_url(self):
         return reverse('url_cust_detail', kwargs={'pk': self.pk})
@@ -25,12 +28,3 @@ class Customers(models.Model):
     class Meta:
         verbose_name_plural = 'Customers'
 
-class Sellers(models.Model):
-    name = models.OneToOneField(User, related_name='sellerName', on_delete=models.CASCADE)
-    salesrep_number = models.IntegerField(blank=True)
-
-    class Meta:
-        verbose_name_plural = 'Sellers'
-
-    def __str__(self):
-        return self.name.first_name + ' ' + self.name.last_name

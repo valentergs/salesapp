23a5d914675a6572fef4bfa78c7bf9a3fc5c53e0
Rodrigo Valente <rodrigovalente@hotmail.com>
2018-06-11 15:00:14 -0300

    add js to base.html

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 from reports.models import Reports
